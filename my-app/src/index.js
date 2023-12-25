c32466c84c256435c933e47538da8460ceccf1ad
codebuildervaibhav <ranavaibhavprf@gmail.com>
2023-12-25 18:53:18 +0530

    done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -42,9 +42,9 @@ const Book = (props) => {
   const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img}  alt="" />
-      <h1>{props.title}</h1>
-      <h4> {props.author} </h4>
+      <img src={img}  alt="" />
+      <h1>{title}</h1>
+      <h4> {author} </h4>
     </article>
   );
 };
